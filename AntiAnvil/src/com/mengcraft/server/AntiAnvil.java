8b83878206009c8cfe4d1a98e67d09c07a1720ae
yiwan <caoli5288@gmail.com>
2014-09-27 20:15:54 +0800

    New module AntiAnvil.

---
+++ AntiAnvil/src/com/mengcraft/server/AntiAnvil.java
@@ -0,0 +1,57 @@
+package com.mengcraft.server;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+
+import java.io.IOException;
+
+/**
+ * Created by zmy on 14-9-27.
+ * GPLv2 licence
+ */
+public class AntiAnvil extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        try {
+            new Metrics(this).start();
+        } catch (IOException e) {
+            getLogger().warning("Can not link to Metrics server!");
+        }
+        String[] strings = {
+                ChatColor.GREEN + "梦梦家高性能服务器出租",
+                ChatColor.GREEN + "淘宝店 http://shop105595113.taobao.com"
+        };
+        Bukkit.getConsoleSender().sendMessage(strings);
+    }
+
+    @EventHandler
+    public void onClick(InventoryOpenEvent event) {
+        boolean isEvent = event.getView().getTopInventory().getType() == InventoryType.ANVIL
+                && !event.getPlayer().hasPermission("anvil.use");
+        if (isEvent) {
+            getServer().getScheduler().runTaskLater(this, new CloseWindow(event.getPlayer()), 1);
+        }
+    }
+
+    private class CloseWindow implements Runnable {
+        private final HumanEntity player;
+
+        public CloseWindow(HumanEntity player) {
+            this.player = player;
+        }
+
+        @Override
+        public void run() {
+            player.closeInventory();
+            getServer().getPlayerExact(player.getName()).sendMessage(ChatColor.RED + "你没有权限使用附魔台");
+        }
+    }
+}
--- BanWorld/src/com/mengcraft/server/BanWorld.java
+++ BanWorld/src/com/mengcraft/server/BanWorld.java
@@ -2,10 +2,12 @@ package com.mengcraft.server;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
 
