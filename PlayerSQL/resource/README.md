0d0920db52ece5ae44ed98ec4870e5cdefd0ee76
yiwan <caoli5288@gmail.com>
2014-10-02 23:29:23 +0800

    New PlayerSQL version.

--- PlayerSQL/resource/README.md
+++ PlayerSQL/resource/README.md
@@ -1,14 +1,18 @@
 New version task:
 It MUST:
-Can sync as name.
-Can sync as UUID.
-Can sync health with dynamic number.
-Can sync exp with fix craft's bug.
-Can sync armors with fix craft's bug.
-Can sync inventory.
-Can sync end chest.
-Can sync potion effect.
-Can option sync.
-Can check data lock.
-Can unlock automatic.
-Can report a error.
+Can sync as name. done. tested.
+Can sync as UUID. done.
+Can sync health with dynamic number. done.
+Can sync food level. done. tested.
+Can sync exp with fix craft's bug. done. tested.
+Can sync armors with fix craft's bug. done. tested.
+Can sync inventory. done. tested.
+Can sync end chest. done. tested.
+Can sync potion effect. done. tested.
+Can option sync. done. tested.
+Can check data lock. done. tested.
+Can unlock automatic. done. tested.
+Can report a error. drop.
+Can timer check database connection. done.
+Can protect copy items. done.
+Can protect loss data when server crash/shutdown. next.
--- PlayerSQL/src/com/mengcraft/server/PlayerSQL.java
+++ PlayerSQL/src/com/mengcraft/server/PlayerSQL.java
@@ -1,5 +1,12 @@
 package com.mengcraft.server;
 
+import com.comphenix.protocol.utility.StreamSerializer;
+import com.earth2me.essentials.craftbukkit.SetExpFix;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -7,81 +14,59 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.mcstats.Metrics;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
 
+/**
+ * GPLv2 licence.
+ */
 public class PlayerSQL extends JavaPlugin {
 
-    private static PlayerSQL plugin = null;
-    private static Connection connection = null;
-    private static PlayerListener listener = null;
-
-    public static Connection getConnection() {
-        return connection;
-    }
-
-    public static PlayerListener getListener() {
-        return listener;
-    }
-
-    public static PlayerSQL getInstance() {
-        return plugin;
-    }
+    private Connection connection;
+    private boolean uuid;
 
     @Override
     public void onLoad() {
         saveDefaultConfig();
-        plugin = this;
     }
 
     @Override
     public void onEnable() {
-        boolean use = getConfig().getBoolean("plugin.use", false);
-        if (use) {
-            try {
-                setConnection();
-                setDataTable();
-                new CheckTask().runTaskTimer(this,
-                        getConfig().getInt("plugin.check", 3000)
-                        , getConfig().getInt("plugin.check", 3000)
-                );
-                listener = new PlayerListener();
-                getServer().getPluginManager().registerEvents(listener, this);
+        try {
+            setConnection();
+            setDataTable();
+            getServer().getScheduler().runTaskTimer(this, new CheckTask(), 3600, 3600);
+            getServer().getPluginManager().registerEvents(new PlayerListener(), this);
 
-                String[] version = getServer().getBukkitVersion().split("-")[0].split("\\.");
-                boolean useUUID = Integer.parseInt(version[1]) > 7
-                        || (Integer.parseInt(version[1]) > 6
-                        && Integer.parseInt(version[2]) > 5);
-                getConfig().set("useUUID", useUUID);
-                getLogger().info("Author: min梦梦");
-                getLogger().info("插件作者: min梦梦");
-            } catch (Exception e) {
-                getLogger().warning("Failed to connect to database");
-                getLogger().warning("Please modify config.yml!!!!!");
-                getServer().getPluginManager().disablePlugin(this);
-                e.printStackTrace();
-            }
-            try {
-                new Metrics(this).start();
-            } catch (Exception e) {
-                getLogger().warning("Failed to connect to mcstats.org");
-            }
-        } else {
-            getLogger().warning("Please modify config.yml!!!");
-            getServer().getPluginManager().disablePlugin(this);
+            String[] version = getServer().getBukkitVersion().split("-")[0].split("\\.");
+            this.uuid = Integer.parseInt(version[1]) > 7
+                    || (Integer.parseInt(version[1]) > 6
+                    && Integer.parseInt(version[2]) > 5);
+            getLogger().info("Author: min梦梦");
+            getLogger().info("插件作者: min梦梦");
+        } catch (Exception e) {
+            getLogger().warning("Failed to connect to database server!");
+            setEnabled(false);
+        }
+        try {
+            new Metrics(this).start();
+        } catch (Exception e) {
+            getLogger().warning("Failed to connect to Metrics server!");
         }
     }
 
     @Override
     public void onDisable() {
-        PlayerManager.saveAll();
+        for (Player player : getServer().getOnlinePlayers()) {
+            new Thread(new SavePlayerTask(player)).start();
+        }
         getLogger().info("Author: min梦梦");
         getLogger().info("插件作者: min梦梦");
     }
@@ -91,64 +76,285 @@ public class PlayerSQL extends JavaPlugin {
         String username = getConfig().getString("plugin.username");
         String password = getConfig().getString("plugin.password");
         try {
-            PlayerSQL.connection = DriverManager.getConnection(database, username, password);
+            this.connection = DriverManager.getConnection(database, username, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void setDataTable() throws SQLException {
-        Statement create = connection.createStatement();
+    private void setDataTable() throws Exception {
+        Statement create = this.connection.createStatement();
         create.execute("CREATE TABLE IF NOT EXISTS PlayerSQL("
                         + "ID int NOT NULL AUTO_INCREMENT, "
                         + "NAME text NOT NULL, "
                         + "DATA text NULL, "
                         + "ONLINE int NULL, "
-                        + "PRIMARY KEY(ID));"
+                        + "PRIMARY KEY(ID)" +
+                        ");"
         );
         try {
-            create.execute("ALTER TABLE `PlayerSQL`"
-                            + "ADD COLUMN `ONLINE` int NULL AFTER `DATA`;"
-            );
+            create.execute("ALTER TABLE `PlayerSQL` ADD COLUMN `ONLINE` int NULL AFTER `DATA`;");
         } catch (Exception e) {
             e.getMessage();
         }
         create.close();
     }
 
-    public class PlayerListener implements Listener {
-        private final ArrayList<Player> playerSet;
+    private class PlayerListener implements Listener {
+        private final HashSet<String> protectNameSet;
 
-        private PlayerListener() {
-            playerSet = new ArrayList<Player>();
+        public PlayerListener() {
+            protectNameSet = new HashSet<String>();
         }
 
-        @EventHandler(priority = EventPriority.MONITOR)
-        public void playerQuit(PlayerQuitEvent event) {
-            if (!playerSet.remove(event.getPlayer())) {
-                PlayerManager.getOnlinePlayer(event.getPlayer()).savePlayer(true);
+        /**
+         * When player quit event fire, save player's data with a new thread.
+         *
+         * @param event PlayerQuitEvent.
+         */
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void playerQuitEvent(PlayerQuitEvent event) {
+            if (this.protectNameSet.contains(event.getPlayer().getName())) {
+                this.protectNameSet.remove(event.getPlayer().getName());
+            } else {
+                new Thread(new SavePlayerTask(event.getPlayer())).start();
             }
         }
 
         @EventHandler(priority = EventPriority.LOWEST)
-        public void playerJoin(PlayerJoinEvent event) {
-            playerSet.add(event.getPlayer());
-            PlayerManager.getOnlinePlayer(event.getPlayer()).loadPlayer();
+        public void playerJoinEvent(PlayerJoinEvent event) {
+            new Thread(new LoadPlayerTask(event.getPlayer())).start();
+
         }
 
-        @EventHandler(priority = EventPriority.LOWEST)
-        public void playerDrop(PlayerDropItemEvent event) {
-            if (playerSet.contains(event.getPlayer())) {
+        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+        public void playerDropItemEvent(PlayerDropItemEvent event) {
+            if (this.protectNameSet.contains(event.getPlayer().getName())) {
                 event.setCancelled(true);
             }
         }
 
-        public void remove(Player player) {
-            playerSet.remove(player);
+        private class LoadPlayerTask implements Runnable {
+            private final String name;
+            private final String uid;
+            private int check;
+
+            public LoadPlayerTask(Player player) {
+                this.name = player.getName();
+                this.uid = player.getUniqueId().toString();
+                this.check = 0;
+                protectNameSet.add(this.name);
+            }
+
+            /**
+             * Database: ID, NAME, DATA, ONLINE
+             */
+            @Override
+            public void run() {
+                try {
+                    PreparedStatement select = connection.prepareStatement("SELECT * FROM `PlayerSQL` WHERE `NAME` = ?;");
+                    select.setString(1, uuid ? this.uid : this.name);
+                    ResultSet result = select.executeQuery();
+                    if (result.next()) {
+                        if (result.getInt(4) < 1) {
+                            loadPlayer(result.getString(3));
+                            lockPlayer(result.getString(2));
+                            protectNameSet.remove(this.name);
+                        } else {
+                            if (check < 4) {
+                                check = check + 1;
+                                run();
+                            } else {
+                                loadPlayer(result.getString(3));
+                                protectNameSet.remove(this.name);
+                                getLogger().warning("Player [" + name + "] 's lock status error!");
+                            }
+                        }
+                    } else {
+                        newPlayer(uuid ? this.uid : this.name);
+                        protectNameSet.remove(this.name);
+                    }
+                    result.close();
+                    select.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            /**
+             * Lock player's online statue.
+             *
+             * @param player Player's name;
+             */
+            private void lockPlayer(String player) throws SQLException {
+                PreparedStatement update = connection.prepareStatement("UPDATE `PlayerSQL` SET `ONLINE` = 1 WHERE `NAME` = ?;");
+                update.setString(1, player);
+                update.executeUpdate();
+                update.close();
+            }
+
+            /**
+             * Load player's data with: [health, food, exp, inventory, armor, chest, effect]
+             *
+             * @param data Data.
+             */
+            private void loadPlayer(String data) {
+                JsonArray array = new JsonParser().parse(data).getAsJsonArray();
+                Player player = getServer().getPlayerExact(this.name);
+                if (player.isOnline()) {
+                    if (getConfig().getBoolean("sync.exp", true)) {
+                        SetExpFix.setTotalExperience(player, array.get(2).getAsInt());
+                    }
+                    if (getConfig().getBoolean("sync.potion", true)) {
+                        for (PotionEffect effect : player.getActivePotionEffects()) {
+                            player.removePotionEffect(effect.getType());
+                        }
+                        player.addPotionEffects(arrayToEffects(array.get(6).getAsJsonArray()));
+                    }
+                    if (getConfig().getBoolean("sync.health", true)) {
+                        try {
+                            player.setHealth(array.get(0).getAsDouble());
+                        } catch (Exception e) {
+                            player.setHealth(20);
+                        }
+                    }
+                    if (getConfig().getBoolean("sync.food", true)) {
+                        player.setFoodLevel(array.get(1).getAsInt());
+                    }
+                    if (getConfig().getBoolean("sync.inventory", true)) {
+                        player.getInventory().setContents(arrayToStacks(array.get(3).getAsJsonArray()));
+                        player.getInventory().setArmorContents(arrayToStacks(array.get(4).getAsJsonArray()));
+                    }
+                    if (getConfig().getBoolean("sync.chest", true)) {
+                        player.getEnderChest().setContents(arrayToStacks(array.get(5).getAsJsonArray()));
+                    }
+                }
+            }
+
+            /**
+             * De serialize PotionEffect from JsonArray.
+             *
+             * @param effectArray JsonArray.
+             * @return Collection<PotionEffect>.
+             */
+            private Collection<PotionEffect> arrayToEffects(JsonArray effectArray) {
+                List<PotionEffect> effectList = new ArrayList<PotionEffect>();
+                for (JsonElement element : effectArray) {
+                    JsonArray array = element.getAsJsonArray();
+                    effectList.add(new PotionEffect(PotionEffectType.getByName(array.get(0).getAsString()), array.get(1).getAsInt(), array.get(2).getAsInt(), array.get(3).getAsBoolean()));
+                }
+                return effectList;
+            }
+
+            /**
+             * De serialize ItemStack from JsonArray.
+             *
+             * @param array Arrays.
+             * @return ItemStacks.
+             */
+            private ItemStack[] arrayToStacks(JsonArray array) {
+                List<ItemStack> stackList = new ArrayList<ItemStack>();
+                StreamSerializer serializer = StreamSerializer.getDefault();
+                try {
+                    for (JsonElement element : array) {
+                        if (element.isJsonNull()) {
+                            stackList.add(new ItemStack(Material.AIR));
+                        } else {
+                            stackList.add(serializer.deserializeItemStack(element.getAsString()));
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return stackList.toArray(new ItemStack[array.size()]);
+            }
+
+            private void newPlayer(String newName) throws SQLException {
+                PreparedStatement insert = connection.prepareStatement("INSERT INTO `PlayerSQL`(`NAME`, `ONLINE`) VALUES(?, 1);");
+                insert.setString(1, newName);
+                insert.executeUpdate();
+                insert.close();
+            }
+        }
+
+    }
+
+    private class SavePlayerTask implements Runnable {
+        private final String name;
+        private final String data;
+
+
+        public SavePlayerTask(Player player) {
+            this.name = uuid ? player.getUniqueId().toString() : player.getName();
+            this.data = getPlayerData(player);
+        }
+
+        /**
+         * Get player's data with: [health, food, exp, inventory, armor, chest, effect]
+         *
+         * @param player The player.
+         * @return The data.
+         */
+        private String getPlayerData(Player player) {
+            Gson json = new Gson();
+            JsonArray array = new JsonArray();
+            array.add(json.toJsonTree(player.getHealth()));
+            array.add(json.toJsonTree(player.getFoodLevel()));
+            array.add(json.toJsonTree(SetExpFix.getTotalExperience(player)));
+            array.add(stacksToArray(player.getInventory().getContents()));
+            array.add(stacksToArray(player.getInventory().getArmorContents()));
+            array.add(stacksToArray(player.getEnderChest().getContents()));
+            array.add(effectsToArray(player.getActivePotionEffects()));
+            return array.toString();
+        }
+
+        private JsonArray effectsToArray(Collection<PotionEffect> effects) {
+            Gson json = new Gson();
+            JsonArray array = new JsonArray();
+            for (PotionEffect effect : effects) {
+                JsonArray elements = new JsonArray();
+                elements.add(json.toJsonTree(effect.getType().getName()));
+                elements.add(json.toJsonTree(effect.getDuration()));
+                elements.add(json.toJsonTree(effect.getAmplifier()));
+                elements.add(json.toJsonTree(effect.isAmbient()));
+                array.add(elements);
+            }
+            return array;
+        }
+
+        private JsonArray stacksToArray(ItemStack[] contents) {
+            Gson json = new Gson();
+            JsonArray array = new JsonArray();
+            StreamSerializer serializer = StreamSerializer.getDefault();
+            try {
+                for (ItemStack content : contents) {
+                    if (content != null && content.getType() != Material.AIR) {
+                        array.add(json.toJsonTree(serializer.serializeItemStack(content)));
+                    } else {
+                        array.add(json.toJsonTree(null));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return array;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PreparedStatement save = connection.prepareStatement("UPDATE `PlayerSQL` SET `DATA` = ?, `ONLINE` = 0 WHERE `NAME` = ?;");
+                save.setString(1, this.data);
+                save.setString(2, this.name);
+                save.executeUpdate();
+                save.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private class CheckTask extends BukkitRunnable {
+    private class CheckTask implements Runnable {
         @Override
         public void run() {
             if (connection != null) {
